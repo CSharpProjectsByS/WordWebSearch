1118c76613e8516b699bc2fedb8e675ada9a08d4
DariuszN <scorpion43@gmail.com>
2017-03-16 12:00:56 +0100

    Wersja do sprawdzenia

--- Wyszukiwarka Słów/WordSearcher.cs
+++ Wyszukiwarka Słów/WordSearcher.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -9,9 +11,44 @@ namespace Wyszukiwarka_Słów
 {
     class WordSearcher
     {
-        public void FindWordsForPages(List<string> words, List<string> pages, ListBox resultView)
+        public async void FindWordsForPages(List<string> words, List<string> pageUrls, ListBox resultView)
         {
-            
+            foreach (string pageUrl in pageUrls)
+            {
+                HttpClient httpClient = new HttpClient();
+                Task<string> getStringTask = httpClient.GetStringAsync(pageUrl);
+
+                resultView.Items.Add(pageUrl);
+
+                string pageContent = await getStringTask;
+
+                List<string> resultsForPage =  FindWordsInPage(words, pageContent).Result;
+
+                AddResultToCollection(resultsForPage, resultView);
+
+            }
+        }
+
+        public async Task<List<String>>  FindWordsInPage(List<string> words, String pageContent)
+        {
+            List<string> results = new List<string>(); 
+            foreach (string word in words)
+            {
+                int count = Regex.Matches(pageContent, word).Count;
+                results.Add(word + ": " + count);
+            }
+
+            return results;
+        }
+
+        private void AddResultToCollection(List<string> results, ListBox resultView)
+        {
+            foreach (string result in results)
+            {
+                resultView.Items.Add(result);
+            }
+
+            resultView.Items.Add("");
         }
     }
 }
